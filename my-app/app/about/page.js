f9df16d029df38f149fbfd24502b7f9f68973d29
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-11-30 16:12:38 +0500

    Linking routing concept in Next.js

---
+++ my-app/app/about/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+function About() {
+  return (
+    <>
+    <h2>About us</h2>
+   <Link href='/home'>Home</Link>
+    </>
+  )
+}
+
+export default About
--- my-app/app/home/page.js
+++ my-app/app/home/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link"
 
 function Home() {
   return (
     <>
     <h2>Home</h2>
+    <Link href='/about'>About</Link>
     </>
   )
 }
